afea028bec3166a178dc77a920e46cfa6ea35d8b
manach <chris.scott22@gmail.com>
2021-05-28 16:33:28 -0400

    working on react state

--- src/templates/ItemPageTemplate.js
+++ src/templates/ItemPageTemplate.js
@@ -5,72 +5,72 @@ import { render } from 'react-dom';
 import styled from 'styled-components';
 import Layout from '../components/Layout';
 
-const Dropdown = styled.select`
-  display: block;
-  padding: 10px;
-  margin: 10px 0;
-  background: ${props => props.theme.colors.secondaryAccent};
-  font-weight: 700;
-  border: none;
-  outline: none;
-`;
+// const Dropdown = styled.select`
+//   display: block;
+//   padding: 10px;
+//   margin: 10px 0;
+//   background: ${props => props.theme.colors.secondaryAccent};
+//   font-weight: 700;
+//   border: none;
+//   outline: none;
+// `;
 
-const DropdownOption = styled.option`
-  padding: 10px;
-  background: ${props => props.theme.colors.secondaryAccent};
-  font-weight: 700;
-  border: none;
-  outline: none;
-`;
+// const DropdownOption = styled.option`
+//   padding: 10px;
+//   background: ${props => props.theme.colors.secondaryAccent};
+//   font-weight: 700;
+//   border: none;
+//   outline: none;
+// `;
 
-class SingleItemPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      threadLengths: '15',
-      threadOptions: 'acrylic',
-    };
-  }
+// class SingleItemPage extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       threadLengths: '15',
+//       threadOptions: 'acrylic',
+//     };
+//   }
 
-  setSelected = value => {
-    this.setState({ selected: value });
-  };
+//   setSelected = value => {
+//     this.setState({ selected: value });
+//   };
 
-  render() {
-    const item = this.props.data.item.node;
+//   render() {
+//     const item = this.props.data.item.node;
 
-    return (
-      <Layout>
-        <h2>SINGLE ITEM</h2>
-        // <p>{item.name}</p>
-        // <GatsbyImage image={image} alt={`A picture of a ${item.name}`} />
-        <Dropdown
-          id="threadLengths"
-          onChange={e => this.setSelected(e.target.value)}
-          value={this.state.selected}
-        >
-          {item.threadLengths.map(option => (
-            <DropdownOption key={option}>{option}</DropdownOption>
-          ))}
-        </Dropdown>
-      </Layout>
-    );
-  }
-}
-// export default function SingleItemPage({ data }) {
-//   const item = data.item;
-//   const image = getImage(item.image.asset.gatsbyImageData);
-
-//   return (
-//     <Layout>
-//       <h2>SINGLE ITEM</h2>
-//       <p>{item.name}</p>
-//       <GatsbyImage image={image} alt={`A picture of a ${item.name}`} />
-//     </Layout>
-//   );
+//     return (
+//       <Layout>
+//         <h2>SINGLE ITEM</h2>
+//         // <p>{item.name}</p>
+//         // <GatsbyImage image={image} alt={`A picture of a ${item.name}`} />
+//         <Dropdown
+//           id="threadLengths"
+//           onChange={e => this.setSelected(e.target.value)}
+//           value={this.state.selected}
+//         >
+//           {item.threadLengths.map(option => (
+//             <DropdownOption key={option}>{option}</DropdownOption>
+//           ))}
+//         </Dropdown>
+//       </Layout>
+//     );
+//   }
 // }
+export default function SingleItemPage({ data }) {
+  const item = data.item;
+  const image = getImage(item.image.asset.gatsbyImageData);
 
-export default SingleItemPage;
+  return (
+    <Layout>
+      <h2>SINGLE ITEM</h2>
+      <p>{item.name}</p>
+      <GatsbyImage image={image} alt={`A picture of a ${item.name}`} />
+    </Layout>
+  );
+}
+
+// export default SingleItemPage;
 
 export const query = graphql`
   query($slug: String!) {
