b177c72a8b09feef139bd7256568ad5437fb0620
manach <chris.scott22@gmail.com>
2021-05-28 14:47:21 -0400

    getting started

--- src/templates/ItemPageTemplate.js
+++ src/templates/ItemPageTemplate.js
@@ -1,5 +1,43 @@
+import { graphql } from 'gatsby';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import React from 'react';
+import Layout from '../components/Layout';
 
-export default function SingleItemPage() {
-  return <p>SINGLE ITEM!!!</p>;
+class SingleItemPage extends React.Component {
+  state = 
 }
+
+export default function SingleItemPage({ data }) {
+  const item = data.item;
+  const image = getImage(item.image.asset.gatsbyImageData);
+
+  return (
+    <Layout>
+      <h2>SINGLE ITEM</h2>
+      <p>{item.name}</p>
+      <GatsbyImage image={image} alt={`A picture of a ${item.name}`} />
+    </Layout>
+  );
+}
+
+export const query = graphql`
+  query($slug: String!) {
+    item: sanityItem(slug: { current: { eq: $slug } }) {
+      id
+      image {
+        asset {
+          gatsbyImageData
+        }
+      }
+      name
+      price
+      threadLengths {
+        threadLengthArray
+      }
+      threadOptions {
+        name
+      }
+      wrist
+    }
+  }
+`;
